a44187a205b8f52619255bf5b0a1e6b5b20bf984
IdaSalek <118059121+IdaSalek@users.noreply.github.com>
2022-11-12 19:20:51 +0100

    Add files via upload

---
+++ kalkulator.java
@@ -0,0 +1,24 @@
+package zajecia;
+
+import java.util.Scanner;
+
+public class kalkulator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println ("Podaj działanie");
+        String działanie;
+        System.out.println("Podaj pierwsza liczbe");
+        int pierwsza_liczba = scanner.nextInt();
+        System.out.println("Podaj druga liczbe");
+        int druga_liczba = scanner.nextInt();
+        if (działanie.equals("x")){
+            break
+        }
+if(działanie.equals("Dodawanie")){
+    System.out.println
+}
+
+    }
+
+
+}
